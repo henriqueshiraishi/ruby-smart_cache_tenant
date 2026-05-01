11fc4f1ea944e1adbbb8e13c1879a0573a5b290f
Henrique A. Shiraishi <henrique@halkitec.com.br>
2026-05-01 16:51:38 -0300

    Initialize smart_cache_tenant gem structure with essential files

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in smart_cache_tenant.gemspec
+gemspec
+
+gem "irb"
+gem "rake", "~> 13.0"
---
+++ lib/smart_cache_tenant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "smart_cache_tenant/version"
+
+module SmartCacheTenant
+  class Error < StandardError; end
+  # Your code goes here...
+end
